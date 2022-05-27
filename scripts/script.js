5e50107f51fc47be46460f283ad41d958b3b63d5
theverybusybee <v_antonchik@mail.ru>
2022-05-27 11:08:59 +0300

    validation added

--- scripts/script.js
+++ scripts/script.js
@@ -69,27 +69,35 @@ addPhotocardButton.addEventListener('click', function () {
   openPopup(popupAddPhoto);
 });
 
-/* -------------------------------- закрытие модального окна -------------------------------*/
+/* ----------------------------- закрытие модального окна ----------------------------*/
 
+function closePopup(popupElement) {
+  popupElement.classList.remove('popup_opened');
+}
 
-popupEditCloseButton.addEventListener('click', function () {
-  closePopup(popupEditProfile);
-});
+// закрываем поп-апы по клику на крестик
+const popupCloseButtons = document.querySelectorAll('.popup__close-button');
+popupCloseButtons.forEach((button) => {
+  button.addEventListener('click', (evt) => {
+    closePopup(evt.target.parentElement.parentElement);
+  })
+})
 
-popupAddCloseButton.addEventListener('click', function () {
-  closePopup(popupAddPhoto);
-});
+// закрываем поп-ап нажатием на еsc
+  page.addEventListener('keydown', function(evt) {
+    if ((evt.key === 'Escape')) {
+      closePopup(page.querySelector('.popup_opened'));
+    };
+  });
 
-popupPhotocardCloseButton.addEventListener('click', function () {
-  closePopup(popupOpenPhotocard);
-});
+// закрываем поп-ап кликом на область вокруг форм
+  page.addEventListener('click', function(evt) {
+    if (evt.target.classList.contains('popup')) {
+       closePopup(evt.target);
+    };
+  });
 
-popupEditAvatarCloseButton.addEventListener('click', function () {
-  closePopup(popupOpenPhotocard);
-});
-
-
-/*------------------------------редактирование информации 'о себе'-----------------------------*/
+/*--------------------------- редактирование информации 'о себе' --------------------------*/
 
 
 // в функции два параметра, которые изменяют текстовое содержимое в username и caption
@@ -168,7 +176,7 @@ initialCards.forEach((element) => {
   cardsContainer.append(photocardElement); // располагаем карточки в начале списка
 });
 
-/* -------------------------------------- добавление карточек ----------------------------------- */
+/* -------------------------------- добавление карточек -------------------------------- */
 
 function addPhotocard(card) {
   const photocardElement = createCard(card);
@@ -184,14 +192,89 @@ function handleCardFormSubmit(evt) {
 
 addPhotoForm.addEventListener('submit', handleCardFormSubmit);
 
-  page.addEventListener('keydown', function(evt) {
-    if ((evt.key === 'Escape')) {
-      closePopup(page.querySelector('.popup_opened'));
-    };
-  });
+/* -------------------------------- валидация форм -------------------------------- */
 
-  page.addEventListener('click', function(evt) {
-    if (evt.target.classList.contains('popup')) {
-       closePopup(evt.target);
-    };
+// показываем сообщение об ошибке
+const showError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__form-item_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__form-item-error_active');
+}
+
+// скрываем сообщение об ошибке
+const hideError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__form-item_type_error');
+  errorElement.textContent = '';
+  errorElement.classList.remove('popup__form-item-error_active');
+}
+
+// указываем кастомные сообщения об ошибке в соответствии с макетом
+const setCustomErrorMessage = (inputElement, errorMessage) => {
+  if(inputElement.type.toString() === 'url') {
+      switch (inputElement.validity.typeMismatch) {
+        case true: errorMessage = 'Введите адрес сайта.';
+        break;
+        case false: errorMessage = '';
+        break;
+      } 
+    } else if (inputElement.validity.valueMissing) {
+      errorMessage = 'Вы пропустили это поле.'
+    } else { 
+      errorMessage = inputElement.validationMessage;
+    }
+    return errorMessage;
+}
+
+// проверяем инпут на валидность и в соответсвии с этим показываем/скрываем сообщения об ошибке
+const checkInputValidaty = (formElement, inputElement) => {
+  let errorWarning = '';
+  if(!inputElement.validity.valid) {
+    showError(formElement, inputElement, setCustomErrorMessage(inputElement, errorWarning));
+  } else {
+    hideError(formElement, inputElement);
+  }
+}
+
+// проверяем все поля формы на валидность
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
   });
+};
+
+// изменяем состояние кнопки в зависимости от валидности инпутов
+const toggleButtonState = (inputList, submitButton) => {
+  if (hasInvalidInput(inputList)) {
+    submitButton.disabled = true;
+  } else {
+    submitButton.disabled = false;
+  }
+}
+
+// устанавливаем обработчики событий на все элементы форм
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__form-item'));
+  const submitButton = formElement.querySelector('.popup__submit-button');
+  toggleButtonState(inputList, submitButton);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function() {
+      checkInputValidaty(formElement, inputElement);
+      toggleButtonState(inputList, submitButton);
+    });
+  });
+};
+
+// устанавливаем слушатель событий для всех форм     
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
