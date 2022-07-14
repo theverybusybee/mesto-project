397c511c282b602534551219ae81f12c66fbe84e
theverybusybee <v_antonchik@mail.ru>
2022-07-14 10:49:14 +0300

    class constant names changed

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,8 +1,8 @@
 export default class Popup {
   constructor(popupSelector) {
-    this._selector = document.querySelector(popupSelector);
+    this._popup = document.querySelector(popupSelector);
     this._page = document.querySelector(".page__container");
-    this._closeButton = this._selector.querySelector(".popup__close-button");
+    this._closeButton = this._popup.querySelector(".popup__close-button");
     this._handleEscClose = this._handleEscClose.bind(this)
   }
 
@@ -13,17 +13,17 @@ export default class Popup {
   };
 
   open() {
-    this._selector.classList.add("popup_opened");
+    this._popup.classList.add("popup_opened");
     document.addEventListener("keydown", this._handleEscClose);
   }
 
   close() {
-    this._selector.classList.remove("popup_opened");
+    this._popup.classList.remove("popup_opened");
     document.removeEventListener("keydown", this._handleEscClose);
   }
 
   _hendleBgClose() {
-    this._selector.addEventListener("click", (evt) => {
+    this._popup.addEventListener("click", (evt) => {
       if (evt.target.classList.contains("popup_opened")) {
         this.close();
       }
